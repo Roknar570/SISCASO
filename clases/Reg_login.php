9d0f9799d8d41e8f53e5fd609332157da11c3e65
Roknar570 <pimentelc75@gmail.com>
2022-12-04 12:24:17 -0400

    cambio 10

--- clases/Reg_login.php
+++ clases/Reg_login.php
@@ -8,21 +8,17 @@
     $telefono   =$_POST['telefono'];
     $email      =$_POST['email'];
     $password   =$_POST['password'];
-    
+    $pureba     =$_POST['valor'];
     $fe_us_in = date("Y-m-d H:i:s");
 
     echo $nombres." ".$apellidos." ".$cedula." ".$telefono." ".$email." ".$password." ".$fe_us_in;
 
     $rs=$obj->Insert("usuarios", 
-    "cod_usuario, nombres, apellidos, cedula, telefono, email, password, Estado, fec_us_in, cod_us_in",
+    "cod_usuario, nombres, apellidos, cedula, telefono, email, password, Activo, fec_us_in, cod_us_in",
     "'admin','".$nombres."','".$apellidos."','".$cedula."','".$telefono."','".$email."','".$password."','true','".$fe_us_in."', 'USU1'");
 
-    var_dump($rs);
-    
-    
     if($rs){
         header('Location: ../index.php');
-        echo' entre';
     }   else{
         echo ' no entre';
     }          
