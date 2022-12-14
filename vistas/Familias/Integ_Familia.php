ea2c5aa59640c910dd2316a5fd7d0d4e96376dcd
Roknar570 <pimentelc75@gmail.com>
2022-12-14 15:49:27 -0430

    CARLOS

--- vistas/Familias/Integ_Familia.php
+++ vistas/Familias/Integ_Familia.php
@@ -7,6 +7,31 @@
     <title>Document</title>
 </head>
 <body>
+    <div class="container">
+        <h1>Articulos</h1>
+        <form id="frmArticulos" enctype="multipart/form-data">
+            <label>Categoria</label>
+            <select class="form-control input-sm" id="categoriaSelect" name="categoriaSelect">
+                <option value="A">Selecciona Categoria</option>
+                <?php while($ver=mysqli_fetch_row($result)): ?>
+                    <option value="<?php echo $ver[0] ?>"><?php echo $ver[1]; ?></option>
+                <?php endwhile; ?>
+            </select>
+            <label>Nombre</label>
+            <input type="text" class="form-control input-sm" id="nombre" name="nombre">
+            <label>Descripcion</label>
+            <input type="text" class="form-control input-sm" id="descripcion" name="descripcion">
+            <label>Cantidad</label>
+            <input type="text" class="form-control input-sm" id="cantidad" name="cantidad">
+            <label>Precio</label>
+            <input type="text" class="form-control input-sm" id="precio" name="precio">
+            <label>Imagen</label>
+            <input type="file" id="imagen" name="imagen">
+            <p></p>
+            <span id="btnAgregaArticulo" class="btn btn-primary">Agregar</span>
+        </form>
+        <div id="tablaArticulosLoad"></div>
+    </div>
     
 </body>
 </html>
